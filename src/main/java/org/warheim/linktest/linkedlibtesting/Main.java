bdc203c17e53ec8ae74eb2bf15f0b29248a0769b
andy1024 <andrzej.maslowski1024@gmail.com>
2015-12-26 01:35:55 +0100

    Test project for linking github repos

---
+++ src/main/java/org/warheim/linktest/linkedlibtesting/Main.java
@@ -0,0 +1,13 @@
+package org.warheim.linktest.linkedlibtesting;
+
+import org.warheim.simplib.OneFunction;
+
+/**
+ *
+ * @author andy
+ */
+public class Main {
+    public static void main(String... args) throws Exception {
+        System.out.println(OneFunction.echo("calling one function from linked library!"));
+    }
+}
